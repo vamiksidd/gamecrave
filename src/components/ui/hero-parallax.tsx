ead4b5a878ebf3ba0b6d322467df06d799c4d69d
vamiq <vamiksiddiqui666@gmail.com>
2024-02-29 15:52:18 +0530

    final

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -105,12 +105,11 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+        GameCrave <br /> studio
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+        We share the latest news , events  and games.
+        
       </p>
     </div>
   );
@@ -133,7 +132,7 @@ export const ProductCard = ({
         x: translate,
       }}
       whileHover={{
-        y: -20,
+        y: -10,
       }}
       key={product.title}
       className="group/product h-96 w-[30rem] relative flex-shrink-0"
@@ -144,8 +143,8 @@ export const ProductCard = ({
       >
         <Image
           src={product.thumbnail}
-          height="600"
-          width="600"
+          height="500"
+          width="500"
           className="object-cover object-left-top absolute h-full w-full inset-0"
           alt={product.title}
         />
